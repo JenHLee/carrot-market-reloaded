9bafb3e0146f487abdd237f1adcaa63ca0aa4e3e
Hyunju Jennie Lee <hj.jennie.lee@gmail.com>
2024-12-17 17:41:50 -0700

    create actions

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,23 +1,37 @@
+"use client";
 import FormButton from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
+import { handleForm } from "./actions";
+import { useActionState } from "react";
 
-export default function LogIn() {
+export default function Login() {
+  //useFormState => useActionState 
+  // 처음에는 action을 호출하면 - initial state와 formData가 호출
+  // 그 다음부터는 이전 action에서 return된 state와 formData가 호출됨
+  const [state, action] = useActionState(handleForm, null);
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Log in with email and password.</h2>
       </div>
-      <form className="flex flex-col gap-3">
-        <FormInput type="email" placeholder="Email" required errors={[]} />
+      <form action={action} className="flex flex-col gap-3">
         <FormInput
-          type="password"
-          placeholder="Password"
+          name="email"
+          type="email"
+          placeholder="Email"
           required
           errors={[]}
         />
-        <FormButton loading={false} text="LogIn account" />
+        <FormInput
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          errors={[state?.errors ?? []]}
+        />
+        <FormButton text="LogIn account" />
       </form>
       <SocialLogin />
     </div>
--- components/form-btn.tsx
+++ components/form-btn.tsx
@@ -1,12 +1,22 @@
+"use client"; // client component = interactive
+
+import { useFormStatus } from "react-dom";
+
 interface FormButtonProps {
-  loading: boolean;
   text: string;
 }
 
-export default function FormButton({ loading, text }: FormButtonProps) {
+export default function FormButton({ text }: FormButtonProps) {
+  // only be used child of form (component 내부에서 사용되어야 함)
+  // it'll automatically find parent form, 
+  // then will figure the form's action status (pending or not)
+  const { pending } = useFormStatus(); 
   return (
-    <button disabled={loading} className="primary-btn h-10 disabled:bg-neutral-400 disabled:text-neutral-300 disabled:cursor-not-allowed">
-      {loading? "Loading..." : text}
+    <button
+      disabled={pending}
+      className="primary-btn h-10 disabled:bg-neutral-400 disabled:text-neutral-300 disabled:cursor-not-allowed"
+    >
+      {pending ? "Loading..." : text}
     </button>
   );
 }
