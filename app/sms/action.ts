7e52b21895b6ae69247d04d0397fe4b17cd1bbdf
Hyunju Jennie Lee <hj.jennie.lee@gmail.com>
2024-12-22 16:47:11 -0700

    add mobile verfication code

--- app/sms/action.ts
+++ app/sms/action.ts
@@ -3,15 +3,36 @@
 import { z } from "zod"
 import validator from "validator" //javascript library 
 
-const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+interface ActionState {
+    token: boolean,
+}
+
+// const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+const phoneSchema = z.string().trim().refine((phone) => validator.isMobilePhone(phone, "en-CA"));
 
 // user input => formData => number => string 
 // 유저가 input하면 formData를 통해 value는 저절로 string으로 바뀜
 // 따라서 아래 스키마에 number로 쓰면 작동되지 않음.
 // coerce(coercion:강제)는 string을 number로 변환하려고 시도하는 것, 즉 문자는 숫자로 변환 불가
-const tokenSchema = z.coerce.number();
+const tokenSchema = z.coerce.number().min(100000).max(999999);
 
 export async function smsLogin(prevState: any, formData: FormData) {
-    console.log(typeof formData.get("token"));
-   
+    // console.log(typeof formData.get("token")); // string
+    // console.log(typeof tokenSchema.parse(formData.get("token"))) // number
+
+    const phone = formData.get("phone");
+    const token = formData.get("token");
+
+    if (!prevState.token) { // first time call this action
+        const result = phoneSchema.safeParse(phone);
+        if (!result.success) {
+            return {
+                token: false,
+            }
+        } else {
+            return {
+                token: true,
+            }
+        }
+    }
 }
--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -1,12 +1,16 @@
-'use client'
+"use client";
 
 import Button from "@/components/button";
 import Input from "@/components/input";
 import { useActionState } from "react";
 import { smsLogin } from "./action";
 
+const initialState = {
+  token: false,
+};
+
 export default function SMSLogin() {
-  const [state, dispatch] = useActionState(smsLogin, null)
+  const [state, dispatch] = useActionState(smsLogin, initialState);
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
@@ -14,18 +18,17 @@ export default function SMSLogin() {
         <h2 className="text-xl">Verify your phone number.</h2>
       </div>
       <form action={dispatch} className="flex flex-col gap-3">
-        <Input
-          name="phone"
-          type="number"
-          placeholder="Phone number"
-          required
-        />
-        <Input
-          name="token"
-          type="number"
-          placeholder="Verification code"
-          required
-        />
+        <Input name="phone" type="text" placeholder="Phone number" required />
+        {state.token ? (
+          <Input
+            name="token"
+            type="number"
+            min={100000}
+            max={999999}
+            placeholder="Verification code"
+            required
+          />
+        ) : null}
         <Button text="Verify" />
       </form>
     </div>
