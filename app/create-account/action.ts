50a8c2e71e7ba88474c8894b6728bca2ed8b6faa
Hyunju Jennie Lee <hj.jennie.lee@gmail.com>
2024-12-23 13:45:45 -0700

    add superRefine

--- app/create-account/action.ts
+++ app/create-account/action.ts
@@ -2,36 +2,12 @@
 import { PASSWORD_MIN_LENGTH, PASSWORD_REGEX, PASSWORD_REGEX_ERROR } from "@/lib/constants";
 import db from "@/lib/db";
 import { z } from "zod";
+import bcrypt from "bcrypt";
+import { redirect } from "next/navigation";
+import getSession from "@/lib/session";
 
 const checkUsername = (username: string) => !username.includes("potato");
-
 const checkPasswords = ({ password, confirm_password }: { password: string, confirm_password: string }) => password === confirm_password;
-
-// DB와 통신하는 function - Zod 안에서 이루어짐
-const checkUniqueUsername = async (username: string) => {
-    const user = await db.user.findUnique({
-        where: {
-            username: username // 유저의 모든 정보를 가지고 오기때문에 유저의 데이터양이 방대하다면 퍼포먼스 좋지 않음
-        },
-        select: {
-            id: true, // 따라서 id만 가져오라고 하는 것: 확인하려는 것 단순히 같은 이름의 유저가 존재하는가 // User should be null
-        }
-    });
-    return !Boolean(user); // Boolean(user) === false => return true (no problem)
-}
-
-// DB와 통신하는 function - Zod 안에서 이루어짐
-const checkUniqueEmail = async (email: string) => {
-    const user = await db.user.findUnique({
-        where: {
-            email: email
-        },
-        select: {
-            id: true, // User should be null
-        }
-    })
-    return !Boolean(user);
-}
 
 const formSchema = z.object({
     username: z.string({
@@ -41,18 +17,55 @@ const formSchema = z.object({
         .toLowerCase()
         .trim()
         // .transform((username) => `🔥${username}`)
-        .refine(checkUsername, "No potatos allowed")
-        .refine(await checkUniqueUsername, "This username is already taken"),
+        .refine(checkUsername, "No potatos allowed"),
+    // .refine(await checkUniqueUsername, "This username is already taken"),
     email: z.string()
         .email()
-        .toLowerCase()
-        .refine(await checkUniqueEmail, "There is an account already registered with the email"),
+        .toLowerCase(),
+    // .refine(await checkUniqueEmail, "There is an account already registered with the email"),
 
     password: z.string()
         .min(PASSWORD_MIN_LENGTH)
         .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
     confirm_password: z.string().min(PASSWORD_MIN_LENGTH),
-}).refine(checkPasswords, { message: "Both passwords should be the same!", path: ["confirm_password"] })
+})
+    .superRefine(async ({ username }, ctx) => {
+        const user = await db.user.findUnique({
+            where: {
+                username,
+            },
+            select: {
+                id: true
+            }
+        });
+        if (user) {
+            ctx.addIssue({
+                code: 'custom',
+                message: "This username is already taken",
+                path: ["username"],
+                fatal: true // this issue가 치명적임
+            })
+            return z.NEVER; // 뒤에 다른 refine이 있어도 실행되지 않음 따라서 치명적인 순으로 refine하면 퍼포먼스 향상 (Abort early)
+        }
+    }).superRefine(async ({ email }, ctx) => {
+        const user = await db.user.findUnique({
+            where: {
+                email,
+            },
+            select: {
+                id: true
+            }
+        });
+        if (user) {
+            ctx.addIssue({
+                code: 'custom',
+                message: "This email is already taken",
+                path: ["email"],
+                fatal: true // this issue가 치명적임
+            })
+            return z.NEVER; // 뒤에 다른 refine이 있어도 실행되지 않음 따라서 치명적인 순으로 refine하면 퍼포먼스 향상 (Abort early)
+        }
+    }).refine(checkPasswords, { message: "Both passwords should be the same!", path: ["confirm_password"] })
 
 export async function createAccount(prevState: any, formData: FormData) {
     const data = {
@@ -63,13 +76,35 @@ export async function createAccount(prevState: any, formData: FormData) {
     };
     const result = await formSchema.safeParseAsync(data); // will await (safeParse / safeParseAsync)
     if (!result.success) {
+        console.log("error: ", result.error.flatten());
         return result.error.flatten();
     } else {
+
         // the result = there is no email/username
-        // hass password
+        // hass password // 12345 => hashFunction(12345):Oneway function => random string but can't go opposite way (NEVER)
+        const hashedPassword = await bcrypt.hash(result.data.password, 12) // how many time run this algorythm - run 12 times, increase the security
+        // console.log("hashedPassword: ", hashedPassword);
+
         // save the user to db using prisma
-        // log the user in
+        const user = await db.user.create({
+            data: {
+                username: result.data.username,
+                email: result.data.email,
+                password: hashedPassword
+            },
+            select: {
+                id: true
+            }
+        })
+        console.log("user: ", user);
+        // log the user in // give the user the session(cookie) (include the info which is {id: } => something random enrypt 암호화)
+        // When the user go to different pages // we got session(cookies) with random => decrpyt 복호화
+        const session = await getSession();
+        session.id = user.id;
+        await session.save();
+
         // redirect user to "/home
+        redirect("/profile");
     }
     // console.log(result.data);
 
--- app/login/actions.ts
+++ app/login/actions.ts
@@ -1,14 +1,30 @@
 "use server";
+import bcrypt  from 'bcrypt';
 import { PASSWORD_MIN_LENGTH, PASSWORD_REGEX, PASSWORD_REGEX_ERROR } from "@/lib/constants";
 import db from "@/lib/db";
 import { z } from "zod";
+import getSession from '@/lib/session';
+import { redirect } from 'next/navigation';
+
+const checkEmailExists = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    }
+  })
+  return Boolean(user) // return if user O => true / X => false
+}
 
 const formSchema = z.object({
   email: z.string({
     required_error: "Password is required"
-  }).email().toLowerCase(),
-  password: z.string().min(PASSWORD_MIN_LENGTH).regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR)
-
+  }).email().toLowerCase().refine(await checkEmailExists, "An account with this email does not exist."),
+  password: z.string()
+  // .min(PASSWORD_MIN_LENGTH)
+  // .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR)
 })
 
 
@@ -20,11 +36,40 @@ export async function login(prevState: any,
     password: formData.get("password"),
   }
 
-  const result = formSchema.safeParse(data);
+  const result = await formSchema.spa(data); // spa === safeParseAsync
   if (!result.success) {
     return result.error.flatten();
   } else {
-  })
-  console.log(result.data)
-}
+    // find the user with the email
+    // if the user is found, check password hash
+    const user = await db.user.findUnique({
+      where: {
+        email: result.data.email
+      },
+      select: {
+        id: true,
+        password: true
+      }
+    })
+
+    // compare password that user sent us vs hashed password in DB
+    const ok = await bcrypt.compare(result.data.password, user!.password ?? "xxxx")
+    console.log("ok: ", ok);
+    // log the user in
+    if(ok) {
+      const session = await getSession();
+      session.id = user!.id
+      redirect("/profile")
+    } else {
+      // Zod인척 Error return
+      return {
+        fieldErrors: {
+          password:["Wrong password."],
+          email: []
+        }
+      }
+    }
+    // redirect "/profile"
+    console.log(result.data)
   }
+}
